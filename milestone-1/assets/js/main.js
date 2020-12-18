dd42e98869240dacbf462fa23fc4d2773cc85680
salvoab <salvatore.abate85@gmail.com>
2020-12-18 17:27:35 +0100

    Inseriti altri dati nell'array contacts

--- milestone-1/assets/js/main.js
+++ milestone-1/assets/js/main.js
@@ -69,7 +69,7 @@ let app = new Vue({
                     ],
                 },
                 {
-                    name: 'Luisa',
+                    name: 'Mario',
                     avatar: '_4',
                     visible: true,
                     messages: [
@@ -81,6 +81,74 @@ let app = new Vue({
                         {
                             date: '10/01/2020 15:50:00',
                             text: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Franco',
+                    avatar: '_5',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '15/05/2020 16:30:55',
+                            text: 'Ciao! Che hai fatto di bello ieri?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '15/05/2020 16:50:00',
+                            text: 'Ciao, sono andato al cinema',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Luisa',
+                    avatar: '_6',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '20/03/2020 15:33:55',
+                            text: 'Ciao, avete studiato qualcosa di nuovo ieri?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '20/03/2020 15:57:00',
+                            text: 'Ciao, abbiamo solo fatto un ripasso generale',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Aldo',
+                    avatar: '_7',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/02/2020 15:32:55',
+                            text: 'Sei stato al nuovo museo?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/02/2020 15:50:00',
+                            text: 'No non ancora, spero di andarci presto',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Giovanni',
+                    avatar: '_8',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '11/01/2020 8:32:55',
+                            text: 'Buona giornata!',
+                            status: 'sent'
+                        },
+                        {
+                            date: '11/01/2020 8:50:00',
+                            text: 'Grazie anche a te!',
                             status: 'received'
                         }
                     ],
